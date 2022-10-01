bcf99a14cfe1abf6976dcf82e492e144550eccd6
vladimir.yaitskyi <yaitskyi.vladimir@gmail.com>
2022-10-01 13:33:52 +0300

    constructro function added

--- Slider/js/index.js
+++ Slider/js/index.js
@@ -1,5 +1,7 @@
-let slider1 = sliderCreator.getNewSlider();
-let slider2 = sliderCreator.getNewSlider();
+let slider1 = new Slider()
+let slider2 = new Slider()
 
 slider1.init('slider1');
 slider2.init('slider2');
+
+console.log(slider1.listOfImages)
--- Slider/js/slider.js
+++ Slider/js/slider.js
@@ -1,47 +1,46 @@
-let sliderCreator = {
-    getNewSlider: function() {
-    let newSlider = {
-        listOfImages: [],
-        showPrevBtn: null,
-        showNextBtn: null,
-        currentImage: null,
-        currentIndex: 0,
+function Slider() {
+    this.listOfImages = [];
+    this.showPrevBtn = null
+    this.showNextBtn = null;
+    this.currentImage = null;
+    this.currentIndex = 0;
 
-        init: function (sliderId) {
-            let thisIsSlider = this;
-            let classSlider = document.getElementById(sliderId)
+    this.init = function (sliderId) {
+        let thisIsSlider = this;
+        let classSlider = document.getElementById(sliderId)
 
-            this.showPrevBtn = classSlider.querySelector('.show-prev-image')
-            this.showNextBtn = classSlider.querySelector('.show-next-image')
-            this.currentImage = classSlider.querySelector('.main-photo')
+        this.showPrevBtn = classSlider.querySelector('.show-prev-image')
+        this.showNextBtn = classSlider.querySelector('.show-next-image')
+        this.currentImage = classSlider.querySelector('.main-photo')
 
-            this.showPrevBtn.addEventListener('click', function(event) {thisIsSlider.onShowPrevBtnClick(event)} , (event) => {event.preventDefault();});
-            this.showNextBtn.addEventListener('click', function(event) {thisIsSlider.onShowNextBtnClick(event)}, (event) => {event.preventDefault();});
-            this.listOfImages.push('img/first.jpeg');
-            this.listOfImages.push('img/second.jpg');
-            this.listOfImages.push('img/third.jpg');
-        },
-
-        onShowPrevBtnClick: function(event) {
-            if (this.currentIndex === 0) {
-                this.currentIndex = this.listOfImages.length;
-            }
-            this.currentIndex -= 1;
-            this.changeImage(this.listOfImages);
-        },
-
-        onShowNextBtnClick: function(event) {
-            this.currentIndex += 1;
-            if (this.currentIndex === this.listOfImages.length) {
-                this.currentIndex = 0;
-            }
-            this.changeImage(this.listOfImages);
-        },
-
-        changeImage: function(listOfImages) {
-            this.currentImage.src = listOfImages[this.currentIndex]
-        },
+        this.showPrevBtn.addEventListener('click', function(event) {thisIsSlider.onShowPrevBtnClick(event)} , (event) => {event.preventDefault();});
+        this.showNextBtn.addEventListener('click', function(event) {thisIsSlider.onShowNextBtnClick(event)}, (event) => {event.preventDefault();});
+        this.listOfImages.push('img/first.jpeg');
+        this.listOfImages.push('img/second.jpg');
+        this.listOfImages.push('img/third.jpg');
     };
-    return newSlider
+
+    this.onShowPrevBtnClick = function(event) {
+        if (this.currentIndex === 0) {
+            this.currentIndex = this.listOfImages.length;
+        }
+        this.currentIndex -= 1;
+        this.changeImage(this.listOfImages);
+    };
+
+    this.onShowNextBtnClick = function(event) {
+        this.currentIndex += 1;
+        if (this.currentIndex === this.listOfImages.length) {
+            this.currentIndex = 0;
+        }
+        this.changeImage(this.listOfImages);
+    };
+
+    this.changeImage = function(listOfImages) {
+        this.currentImage.src = listOfImages[this.currentIndex]
+    };
 }
-}
+
+
+
+
